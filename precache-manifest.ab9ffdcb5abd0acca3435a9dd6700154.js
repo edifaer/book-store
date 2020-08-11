a4fc87eca273284d0cc86ccda03109ad854a0a28
Kostiantyn Berehovyi <edifaer29122@gmail.com>
2020-08-11 13:19:34 +0300

    Updates

---
+++ precache-manifest.ab9ffdcb5abd0acca3435a9dd6700154.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16b27dc9c3b5939ef43460984fe7431",
+    "url": "/book-store/index.html"
+  },
+  {
+    "revision": "ed4aef2b9730b8a2961a",
+    "url": "/book-store/static/css/3.edb5439c.chunk.css"
+  },
+  {
+    "revision": "415476e5f5a0056e76a0",
+    "url": "/book-store/static/css/4.973888d2.chunk.css"
+  },
+  {
+    "revision": "3cd0691e50e28f0c15f5",
+    "url": "/book-store/static/css/main.29dc17c6.chunk.css"
+  },
+  {
+    "revision": "0d8282775a2b01518088",
+    "url": "/book-store/static/js/2.ec1e5ab1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/book-store/static/js/2.ec1e5ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed4aef2b9730b8a2961a",
+    "url": "/book-store/static/js/3.d697cbf9.chunk.js"
+  },
+  {
+    "revision": "415476e5f5a0056e76a0",
+    "url": "/book-store/static/js/4.b83c80ce.chunk.js"
+  },
+  {
+    "revision": "3cd0691e50e28f0c15f5",
+    "url": "/book-store/static/js/main.74971ea4.chunk.js"
+  },
+  {
+    "revision": "97e47df63912297a84c4",
+    "url": "/book-store/static/js/runtime-main.cacc82f8.js"
+  }
+]);
